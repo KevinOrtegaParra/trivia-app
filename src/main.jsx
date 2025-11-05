23b427db2572b6c9f7c2bb17723a8dc0f05be85e
Kevin Ortega <kevinantonioortegaparra10@gmail.com>
2025-11-04 21:19:54 -0500

    feat: agregar pantalla para seleccionar y actualizar grado del usuario después del login con Google

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="168345343263-hh5gip6jrgva2nme7p2p8t4oghan94h5.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={VITE_GOOGLE_CLIENT_ID}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
